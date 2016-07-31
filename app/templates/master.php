83f5b5d524ef77be7491c2da04ec20f6a03e0e68
rscarlett1 <raewynne.scarlett@gmail.com>
2016-08-01 10:32:53 +1200

    home page gone accross to php and index html still here

---
+++ app/templates/master.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+
+<html lang="en-nz">
+
+<head>
+		<meta charset="utf8">
+
+		<title></title>
+
+		<meta name="description" content="Between 150 and 160 characters">
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+		<link rel="stylesheet" href="css/styles.css">
+
+		<link rel="stylesheet" href="font-awesome-4.6.3/css/font-awesome.min.css">
+
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+
+
+
+
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+<script type="text/javascript">
+	$('.carousel').carousel();
+</script>
+</body>
+</html>
